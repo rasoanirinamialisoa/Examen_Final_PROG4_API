b80f05cbcc2ef6fa36b716d8b06daf5d9a5c64ac
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-03-13 21:41:11 +0300

    init commit

---
+++ src/main/java/com/devunited/examenfinalprog4/ExamenFinalProg4Application.java
@@ -0,0 +1,13 @@
+package com.devunited.examenfinalprog4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalProg4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalProg4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devunited/examenfinalprog4/ExamenFinalProg4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devunited.examenfinalprog4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalProg4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
