981c57feb8d3abfe026e648a3ea935b4c1ee600b
DinahFitiavana <hei.dinah.3@gmail.com>
2024-03-18 15:17:39 +0300

    add script to insert mock data to table transfers

--- src/main/resources/script_sql/transfers.sql
+++ src/main/resources/script_sql/transfers.sql
@@ -8,3 +8,11 @@ CREATE TABLE "transfers"(
     other_bank_name           Varchar (50) NOT NULL,
     id_accounts               INT NOT NULL REFERENCES accounts(id)
 );
+
+
+INSERT INTO "transfers" (to_account_id, amount, same_bank, other_bank, other_bank_name, id_accounts) VALUES
+('2345678901', 100.00, false, true, 'BOA', 1),
+('3456789012', 150.00, true, false, '', 2),
+('4567890123', 200.00, false, true, 'BNI', 3),
+('5678901234', 250.00, false, true, 'BMOI', 4),
+('6789012345', 300.00, true, false, '', 5);
