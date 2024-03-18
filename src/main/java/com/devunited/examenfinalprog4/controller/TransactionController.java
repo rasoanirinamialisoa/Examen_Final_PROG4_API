7d0a095aab50f76fa60179ef362755e8f514fc9f
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-03-18 13:39:49 +0300

    feat:add & configure crudoperations for Transactions entity

---
+++ src/main/java/com/devunited/examenfinalprog4/controller/TransactionController.java
@@ -0,0 +1,39 @@
+package com.devunited.examenfinalprog4.controller;
+
+import com.devunited.examenfinalprog4.model.Transactions;
+import com.devunited.examenfinalprog4.service.TransactionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("/transactions")
+    public List<Transactions> getAllTransactions() throws SQLException {
+        return transactionService.getAllTransactions();
+    }
+
+    @GetMapping("/transactions/{id}")
+    public Transactions getTransactionById(@PathVariable int id) throws SQLException {
+        return transactionService.getTransactionById(id);
+    }
+
+    @PostMapping("/transactions")
+    public Transactions addTransaction(@RequestBody Transactions transaction) throws SQLException {
+        return transactionService.createTransaction(transaction);
+    }
+
+    @PutMapping("/transactions/{id}")
+    public Transactions updateTransaction(@PathVariable int id, @RequestBody Transactions transaction) throws SQLException {
+        return transactionService.updateTransaction(id, transaction);
+    }
+}
