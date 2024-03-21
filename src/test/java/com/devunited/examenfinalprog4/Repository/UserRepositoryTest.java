a91a6b6e5984f3cca0dcf803a1f59adc53081ff1
Astna17 <hei.antsa.17@gmail.com>
2024-03-21 04:19:38 +0300

    fix: change the methode testGetUserById

--- src/test/java/com/devunited/examenfinalprog4/Repository/UserRepositoryTest.java
+++ src/test/java/com/devunited/examenfinalprog4/Repository/UserRepositoryTest.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -35,7 +36,7 @@ public class UserRepositoryTest {
     }
 
     @Test
-    public void testGetAllUsers() throws SQLException {
+    public void testGetAllUsers(){
         try {
         List<Users> expectedUsers = new ArrayList<>();
         Users user1 = new Users();
@@ -43,8 +44,8 @@ public class UserRepositoryTest {
         user1.setName("John");
         user1.setUsername("john_doe");
         user1.setBirthday(LocalDate.of(1995, 5, 25));
-        user1.setEmail("john@gmail.com");
-        user1.setPassword("johnpassword");
+        user1.setEmail("john@example.com");
+        user1.setPassword("john0147");
         expectedUsers.add(user1);
 
         Users user2 = new Users();
@@ -52,8 +53,8 @@ public class UserRepositoryTest {
         user2.setName("Alice");
         user2.setUsername("alice_smith");
         user2.setBirthday(LocalDate.of(2000, 10, 20));
-        user2.setEmail("alice@gmail.com");
-        user2.setPassword("alicepassword");
+        user2.setEmail("alice@example.com");
+        user2.setPassword("alice582");
         expectedUsers.add(user2);
 
         when(resultSet.next()).thenReturn(true, true, false);
@@ -62,18 +63,48 @@ public class UserRepositoryTest {
         when(resultSet.getString("username")).thenReturn("john_doe", "alice_smith");
         when(resultSet.getDate("birthday")).thenReturn(Date.valueOf(LocalDate.of(1995, 5, 25)),
                 Date.valueOf(LocalDate.of(2000, 10, 20)));
-        when(resultSet.getString("email")).thenReturn("john@gmail.com", "alice@gmail.com");
-        when(resultSet.getString("password")).thenReturn("johnpassword", "alicepassword");
+        when(resultSet.getString("email")).thenReturn("john@example.com", "alice@example.com");
+        when(resultSet.getString("password")).thenReturn("john0147", "alice582");
 
         List<Users> actualUsers = userRepository.getAllUsers();
 
-        Assertions.assertEquals(expectedUsers.size(), actualUsers.size());
+        assertEquals(expectedUsers.size(), actualUsers.size());
         for (int i = 0; i < expectedUsers.size(); i++) {
-            Assertions.assertEquals(expectedUsers.get(i), actualUsers.get(i));
+            assertEquals(expectedUsers.get(i), actualUsers.get(i));
         }
         } catch (SQLException e) {
             e.printStackTrace();
-            // Handle the exception...
         }
     }
+
+    @Test
+    public void testGetUserById() {
+        try {
+            int userId = 1;
+            Users expectedUser = new Users();
+            expectedUser.setId(userId);
+            expectedUser.setName("Veronica Lodge");
+            expectedUser.setUsername("veronica");
+            expectedUser.setBirthday(LocalDate.of(1997, 11, 18));
+            expectedUser.setEmail("lodgevr@example.com");
+            expectedUser.setPassword("veronica1032");
+
+            when(resultSet.next()).thenReturn(true);
+            when(resultSet.getInt("id")).thenReturn(userId);
+            when(resultSet.getString("name")).thenReturn("John");
+            when(resultSet.getString("username")).thenReturn("john_doe");
+            when(resultSet.getDate("birthday")).thenReturn(Date.valueOf(LocalDate.of(1990, 5, 15)));
+            when(resultSet.getString("email")).thenReturn("john@example.com");
+            when(resultSet.getString("password")).thenReturn("password");
+
+            Users actualUser = userRepository.getUserById(userId);
+
+            assertEquals(expectedUser, actualUser);
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+
 }
