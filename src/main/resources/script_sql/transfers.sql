5fdef93bfb5ac159b364533db626b573d510e3c3
DinahFitiavana <hei.dinah.3@gmail.com>
2024-03-25 12:20:05 +0300

    feat: add the SQl function for the cash_withdrawal

--- src/main/resources/script_sql/transfers.sql
+++ src/main/resources/script_sql/transfers.sql
@@ -11,8 +11,8 @@ CREATE TABLE "transfers"(
 
 
 INSERT INTO "transfers" (to_account_id, amount, same_bank, other_bank, other_bank_name, id_accounts) VALUES
-('2345678901', 100.00, false, true, 'BOA', 1),
-('3456789012', 150.00, true, false, '', 2),
-('4567890123', 200.00, false, true, 'BNI', 3),
-('5678901234', 250.00, false, true, 'BMOI', 4),
-('6789012345', 300.00, true, false, '', 5);
+('23456789', 100.00, false, true, 'BOA', 1),
+('34567890', 150.00, true, false, '', 2),
+('45678901', 200.00, false, true, 'BNI', 3),
+('56789012', 250.00, false, true, 'BMOI', 4),
+('67890123', 300.00, true, false, '', 5);
